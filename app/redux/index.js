3081fad1ecb243d393a3c4daacf0f26338385e90
John Sniezek <john.sniezek@resonate.com>
2017-11-15 10:05:32 -0800

    Adding redux dev tools enhancer

--- app/redux/index.js
+++ app/redux/index.js
@@ -1,10 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { createEpicMiddleware } from 'redux-observable';
 
 import epic from './epic';
 import reducer from './reducer';
 
+// redux devtools store enhancer
+const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default function() {
   const epicMiddleware = createEpicMiddleware(epic);
-  return createStore(reducer, applyMiddleware(epicMiddleware));
+  return createStore(reducer, enhancer(applyMiddleware(epicMiddleware)));
 }
